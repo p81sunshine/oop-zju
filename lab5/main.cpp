56e282dadf2dea7a1397c1338469af0b4294c12d
Jiaxing Liu <lsnfzj@gmail.com>
2023-05-11 17:04:24 +0800

    Fix bug
    构造函数只有在 初始化的时候执行，也就是 Fraction f1 = f2
    
    这样才会执行构造函数。倘若， Fraction f1, f2; f1 = f2;
    
    这样并不会执行构造函数

--- lab5/main.cpp
+++ lab5/main.cpp
@@ -3,16 +3,16 @@
 #include <string>
 
 int main() {
-    Fraction f1(2, 3), f2(3, 4), f3, f4;
+    Fraction f1(2, 3), f2(3, 4);
     std::cout << "f1 = " <<  f1 << " f2 = " << f2 << '\n';
-    f3 = f1;
+    Fraction f3 = f1;
     std::cout << "f3 = f1, f3 = " << f3 << '\n';
 
     std::cout << "f3 = f1+-*/f2 (The following four number) \n";
     f3 = f1 + f2;
     std::cout << f3 << " = " << static_cast<double>(f3) << '\n';
 
-    f3 = f2 - f1;
+    f3 = f1 - f2;
     std::cout << f3 << " = " << static_cast<double>(f3) << '\n';
 
     f3 = f1 * f2;
